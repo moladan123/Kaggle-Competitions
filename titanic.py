e3b0aad3feb95a349fcdd311b6d99e90a715728d
moladan123 <alon.djurinsky@gmail.com>
2019-03-28 16:51:58 -0400

    changed to notebook, added simple regression

--- titanic.py
+++ titanic.py
@@ -2,7 +2,21 @@ import pandas as pd
 
 ROOT = "/Volumes/Unititled/Jan-Apr-2019/Random/KaggleCompetitions/titanicData/titanic/"
 
-info = pd.read_csv(ROOT + "gender_submission.csv")
+sample_prediction = pd.read_csv(ROOT + "gender_submission.csv")
+
 test = pd.read_csv(ROOT + "test.csv")
 train = pd.read_csv(ROOT + "train.csv")
+
+print(train)
+train = train.loc['Survived']
+print(train)
+
+
+inputs = []
+for i in range(test.shape[0]):
+    new_person = train.iloc[i, :]
+
+
+    inputs.append(new_person)
+print(inputs)
 
